0564d151c39eec78c8584550cbccf33f3209dd47
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r4s2.42madrid.com>
2023-11-28 18:21:05 +0100

    first functions

--- src/pipex.c
+++ src/pipex.c
@@ -6,217 +6,36 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:44:40 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/22 17:25:39 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/28 17:32:58 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	int i;
+	int		pipe_fd[2];
+	pid_t	pid;
+	pid_t	parent_pid;
 
-	i = 0;
-	if (argc != 5)
+	if (argc != 5 || pipe(pipe_fd) == -1)
 		ft_error();
-	while (argv[0][i])
-		i++;
-	ft_init_pipe();
-	// ft_execve();
-}
-
-// //*DUP2
-// #include <stdio.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-
-// int main()
-// {
-// 	int fd;
- 
-// 	fd = open("./src/example.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-// 	dup2(fd, STDOUT_FILENO);
-// 	close(fd);
-// 	printf("This is printed in example.txt!\n");
- 
-// 	return (0);
-// }
-
-//*ACCESS
-// #include <stdio.h>
-// #include <unistd.h>
- 
-// int main()
-// {
-// 	if (access("./src/example.txt", R_OK) != -1)
-// 		printf("I have permission\n");
-// 	else
-// 		printf("I don't have permission\n");
- 
-// 	return (0);
-// }
-
-//* EXECVE
-// #include <unistd.h>
-// #include <stdio.h>
- 
-// int main()
-// {
-// 	char *args[3];
- 
-// 	args[0] = "ls";
-// 	args[1] = "-l";
-// 	args[2] = NULL;
-// 	execve("/bin/ls", args, NULL);
-// 	printf("This line will not be executed.\n");
- 
-// 	return (0);
-// }
-
-//* FORK
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
- 
-// int main()
-// {
-// 	pid_t pid;
- 
-// 	pid = fork();
-// 	if (pid == -1)
-// 	{
-// 		perror("fork");
-// 		exit(EXIT_FAILURE);
-// 	}
- 
-// 	if (pid == 0)
-// 		printf("This is the child process. (pid: %d)\n", getpid());
-// 	else
-// 		printf("This is the parent process. (pid: %d)\n", getpid());
- 
-// 	return (0);
-// }
-
-//*FORK + EXECVE
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
- 
-// int main()
-// {
-// 	pid_t pid;
-// 	char *args[3];
- 
-// 	pid = fork();
-// 	// pid = -1;
-// 	if (pid == -1)
-// 	{
-// 		perror("fork");
-// 		exit(EXIT_FAILURE);
-// 	}
-//  	args[0] = "ls";
-// 	args[1] = "-la";
-// 	args[2] = NULL;
-// 	if (pid == 0)
-// 		printf("This is the child process. (pid: %d)\n", getpid());
-// 	else
-// 		printf("This is the parent process. (pid: %d)\n", getpid());
-// 	execve("/bin/ps", args, NULL);
- 
-// 	return (0);
-// }
-
-//*PIPE
-/* 
-The array pipefd is used to
-return two file descriptors referring to the ends of the pipe.
-pipefd[0] refers to the read end of the pipe.  pipefd[1] refers
-to the write end of the pipe.
-*/
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
- 
-int main()
-{
-	int fd[2];
-	pid_t pid;
-	char buffer[13];
- 
-	if (pipe(fd) == -1)
-	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
- 
 	pid = fork();
 	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
- 
+		ft_error();
 	if (pid == 0)
-	{
-		close(fd[0]); // close the read end of the pipe		
-		printf("a\n");
-		write(fd[1], "Hello parent!", 13);
-		close(fd[1]); // close the write end of the pipe
-		exit(EXIT_SUCCESS);
-	}
+		child_process(argv, pipe_fd, envp);
 	else
 	{
-		close(fd[1]); // close the write end of the pipe
-		read(fd[0], buffer, 13);
-		close(fd[0]); // close the read end of the pipe
-		printf("Message from child:'%s'\n", buffer);
-		exit(EXIT_SUCCESS);
+		parent_pid = fork();
+		if (parent_pid != -1)
+			ft_error();
+		if (parent_pid == 0)
+			parent_process(argv, pipe_fd, envp);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		waitpid(pid, NULL, 0);
+		waitpid(parent_pid, NULL, 0);
 	}
-}  */
-
-//*UNLINK
-/* 
-#include <stdio.h>
-#include <unistd.h>
- 
-int main()
-{
-	if (unlink("./src/example.txt") == 0)
-		printf("File successfully deleted");
-	else
-		printf("Error deleting file");
- 
 	return (0);
-} */
-
-//*WAIT
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
- 
-int main(void)
-{
-	pid_t pid;
- 
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		printf("I am the child process.\n");
-		sleep(2);
-		exit(EXIT_SUCCESS);
-	}
-	else
-	{
-		printf("I am the parent process.\n");
-		wait(NULL);
-		printf("Child process terminated after a 2s delay.\n");
-	}
- 
-	return (EXIT_SUCCESS);
-} */
+}
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:03:27 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/22 17:15:47 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/28 18:19:57 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,43 @@ void	ft_error(void)
 	exit(EXIT_FAILURE);
 }
 
-void	ft_init_pipe(void)
+void	execute_command(char **argv, char **envp)
 {
-	int		pipe_fd[2];
-	pid_t	pid;
-	// char	buf;
-	
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	// int fd = open("jajaja", O_RDONLY);
-	// if (fd < 0)
-	// {
-	// 	perror("open");
-	// 	printf("pepe\n");
-	// 	exit(EXIT_FAILURE);
-	// }
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	if (pid == 0)
-	{
-		ft_printf("Hijo\n");
-	}
-	else
-	{
-		ft_printf("Padre\n");
-	}
+	char	**cmd;
+
+	cmd = ft_split(argv, " ");
+
+	if (execv(, cmd, envp) == -1)
+		ft_error();
+
+}
+
+void	child_process(char **argv, int *fd, char **envp)
+{
+	int	file_fd;
+
+	file_fd = open(argv[1], O_RDONLY);
+	if (file_fd == -1)
+		ft_error();
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		ft_error();
+	if (dup2(file_fd, STDIN_FILENO))
+		ft_error();
+	close(fd[1]);
+	execute_command(argv[2], envp);
+}
+
+void	parent_process(char **argv, int *fd, char **envp)
+{
+	int	file_fd;
+
+	file_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (file_fd == -1)
+		ft_error();
+	if (dup2(fd[0], STDIN_FILENO) == 1)
+		ft_error();
+	if (dup2(file_fd, STDOUT_FILENO))
+		ft_error();
+	close(fd[0]);
+	execute_command(argv[3], envp);
 }
