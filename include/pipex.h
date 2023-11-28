dad5a1d74335fe2a2f789c7270b9112052e4c753
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r4s2.42madrid.com>
2023-11-28 18:20:35 +0100

    doing it

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:44:03 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/11/22 15:37:03 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/11/28 17:44:58 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include "../libft/ft_printf.h"
 # include "../libft/get_next_line.h"
 
-void	ft_init_pipe(void);
+void	child_process(char **argv, int *fd, char **envp);
+void	parent_process(char **argv, int *fd, char **envp);
+void	execute_command(char **argv, char **envp);
 void	ft_error(void);
 
 #endif
