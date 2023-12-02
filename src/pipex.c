3985d27bf3d16c7380a87f8f816d844581bd8e50
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r14s2.42madrid.com>
2023-12-02 16:02:51 +0100

    normi

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:44:40 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/12/01 17:49:06 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/12/02 16:02:22 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*get_cmd_route(char *path, char	*cmd)
 	aux = ft_strtrim(path, "PATH=");
 	possible_path = ft_split(aux, ':');
 	if (access(cmd, F_OK | X_OK) == 0)
-		return(cmd);
+		return (cmd);
 	while (possible_path[++i])
 	{
 		aux_route = ft_strjoin(possible_path[i], "/");
@@ -37,19 +37,16 @@ char	*get_cmd_route(char *path, char	*cmd)
 			free(cmd_route);
 		}
 	}
-	free(aux);
+	free (aux);
 	return (cmd_route);
 }
-/* void leaks(void)
-{
-	system("leaks -q pipex");
-} */
+
 int	main(int argc, char **argv, char **envp)
 {
 	int		pipe_fd[2];
 	pid_t	pid;
 	pid_t	parent_pid;
-	//  atexit(leaks);
+
 	if (argc != 5 || pipe(pipe_fd) == -1)
 		ft_error();
 	pid = fork();
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: fdiaz-gu <fdiaz-gu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 13:03:27 by fdiaz-gu          #+#    #+#             */
-/*   Updated: 2023/12/01 17:40:44 by fdiaz-gu         ###   ########.fr       */
+/*   Updated: 2023/12/02 16:02:37 by fdiaz-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ void	parent_process(char **argv, int *fd, char **envp)
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 		ft_error();
 	if (dup2(file_fd, STDOUT_FILENO) == -1)
-		ft_error();	
+		ft_error();
 	close(fd[0]);
 	execute_command(argv[3], envp);
 }
